a8c206bad3fa1e4a5310224ab682abd3e49ccb8c
francesco-borsatti <francesco.bors@gmail.com>
2023-02-18 10:55:12 +0100

    import statements at top of the file

--- export_notebook.py
+++ export_notebook.py
@@ -1,17 +1,34 @@
+#### THIS FILE WAS AUTOMATICALLY GENERATED - DO NOT EDIT ####
+#### Edit source notebook instead: ./export_notebook.ipynb
 import json
 import os
+import ast
+
+def line_contains_import(line):
+    try:
+        parsed = ast.parse(line.strip())
+        for node in ast.walk(parsed):
+            if isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
+                return True
+    except SyntaxError:
+        pass
+    return False
+
 
 def export_notebook(notebook_path:str, 
                     out_dir_path:str = '', 
-                    out_file_name:str = '') -> None:
+                    out_file_name:str = '',
+                    put_imports_on_top:bool = True) -> None:
     """
     Given a notebook file path, exports only code cells whose first line
     is: #_CELL_TO_EXPORT_#
     
-    The output directory path is optional, if none is chose it defaults
+    - The output directory path is optional, if none is chose it defaults
     to the same directory.
-    The output file name is optional, if none is chose it defaults to the
+    - The output file name is optional, if none is chose it defaults to the
     same name of the notebook (but with .py extension).
+    - Put imports on top set to true if you want all the imports to be placed
+    at the top of the script file.
 
     Example use: export_notebook(notebook_path='./this_notebook.ipynb',
                         output_dir_path='../folder/',
@@ -46,8 +63,7 @@ def export_notebook(notebook_path:str,
 
     confirm_export = input('Are you sure you want to export' +
                         f'the notebook [{notebook_path}]' + 
-                        f'to [{out_file_path}] ? (y/N)')
-
+                        f'to [{out_file_path}] ? (y/N) \n')
     if confirm_export.lower() not in ['y', 'yes']:
         print(RED('Export aborted.'))
         return
@@ -57,24 +73,44 @@ def export_notebook(notebook_path:str,
     
     # output file buffer
     buffer = []
+    # place a warning message at the top of the file
+    buffer.append( '#### THIS FILE WAS AUTOMATICALLY GENERATED - DO NOT EDIT ####\n')
+    buffer.append(f'#### Edit source notebook instead: {notebook_path}\n')
+
+    # keep track of the number of imports in the file
+    # to place them in order at the top
+    num_of_imports = len([c for c in buffer if '\n' in c])
 
     with open(notebook_path, 'r') as f:
         data = json.load(f)
         for cell in data['cells']:
-            if cell['cell_type'] == 'code':
-                # if the first line contains the keyword that indicates 
-                # the cell needs to be exported
-                try:
-                    if '#_CELL_TO_EXPORT_#' in cell['source'][0]:
-                            # skip the first line
-                            for line in cell['source'][1:]:
-                                buffer.append(line)
-                            buffer.append('\n')
-                except IndexError:
-                    pass
+            # skip non-code cells
+            if cell['cell_type'] != 'code':
+                continue
+            # skip empty cells
+            if not cell['source']:
+                continue
+            # skip non marked cells
+            if '#_CELL_TO_EXPORT_#' not in cell['source'][0]:
+                continue
+            # -- Cell to be exported -- read line by line:
+            # skip the first line (which contains #_CELL_TO_EXPORT_#)
+            for line in cell['source'][1:]:
+                # if line contains import statement
+                if line_contains_import(line) and put_imports_on_top:
+                    buffer.insert(num_of_imports, line)
+                    num_of_imports = num_of_imports + 1
+                    continue
+                # normal lines are appended at the end
+                buffer.append(line)
+            # put a new line at the end of cells
+            buffer.append('\n')
 
+    # write buffer to output file
     with open(out_file_path, 'w') as out_file:
         for line in buffer:
             out_file.write(line)
-               # [[[[ EXPORT NOTEBOOK TO PY ]]]]
+    
+    # process ended
     print('[-----', GREEN('Export  completed') ,'-----]')
+
